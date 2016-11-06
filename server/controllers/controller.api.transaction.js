cb1997f2b155f0a77da929d3de250a07ec414f53
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-06 18:31:45 +0700

    create add and get structure in transaction controller

---
+++ server/controllers/controller.api.transaction.js
@@ -0,0 +1,19 @@
+const Transaction = require('../models/models.transaction')
+
+module.exports = {
+    addTransaction: function (req, res) {
+
+    },
+
+    getTransaction: function (req, res) {
+
+    },
+
+    getTransactionById: function (req, res) {
+
+    },
+
+    getTransactionByUserId: function (req, res) {
+
+    }
+}
