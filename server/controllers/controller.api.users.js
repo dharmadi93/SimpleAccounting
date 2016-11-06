bec43008b26f883672155b664b71e130b156fce4
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-06 13:03:08 +0700

    add crud structure controller for users

---
+++ server/controllers/controller.api.users.js
@@ -0,0 +1,19 @@
+const User = require('../models/models.user')
+
+module.exports = {
+    addUser: function (req, res) {
+
+    },
+    getUsers: function (req, res) {
+
+    },
+    getUsersById: function (req, res) {
+
+    },
+    deleteUser: function (req, res) {
+
+    },
+    updateUser: function (req, res) {
+
+    }
+}
