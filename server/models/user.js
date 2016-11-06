1315ab2abc50d3626631b36843b4576e402d5548
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-06 12:31:27 +0700

    created user schema

---
+++ server/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const User = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required
+    },
+    email: {
+        type: String,
+        validate: {
+            validator: function(email) {
+                return /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/.test(email)
+            },
+            message: `format email salah`
+        },
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    transaction: []
+})
