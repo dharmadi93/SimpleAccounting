7d940f863c2700589de5f65b7c25236fbba61d84
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-06 19:05:36 +0700

    create crud for role

---
+++ server/controllers/controller.api.role.js
@@ -0,0 +1,23 @@
+const Role = require('../models/models.role')
+
+module.exports = {
+    addRole: function (req, res) {
+
+    },
+
+    getRole: function (req, res) {
+
+    },
+
+    getRoleById: function (req, res) {
+
+    },
+
+    deleteRole: function (req, res) {
+
+    },
+
+    updateRole: function (req, res) {
+
+    }
+}
--- server/routes/routes.roles.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by dharmadi93 on 11/6/16.
- */
